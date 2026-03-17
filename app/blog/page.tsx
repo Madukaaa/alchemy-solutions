c81baa17b304c5e09cc7ada378abeeb54dbf5d19
Madukaaa <madukakarunathilake2003@gmail.com>
2026-03-17 10:22:45 +0530

    Initial Commit

---
+++ app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+  return (
+    <div className="min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl font-bold mb-4">Blog</h1>
+      <p className="text-lg">Welcome to the Blog page.</p>
+    </div>
+  );
+}
