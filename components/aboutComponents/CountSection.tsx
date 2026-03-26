7e3de7350877529a878d894ebb3a7ef37b5a9251
Geilee Gehara <dvggehara@gmail.com>
2026-03-26 12:25:19 +0530

    change inveiw spring the count

--- components/aboutComponents/CountSection.tsx
+++ components/aboutComponents/CountSection.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import CountUp from "react-countup";
-import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
 
 const stats = [
@@ -12,17 +11,15 @@ const stats = [
 ];
 
 export default function CountSection() {
-  const { ref, inView } = useInView({ triggerOnce: false, threshold: 0.3 });
-
   return (
     <div className="bg-black text-white px-6 md:px-16 lg:px-32 py-16">
-      <div ref={ref} className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-20">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-20">
         <div className="grid grid-cols-2 gap-y-10 text-center">
           {stats.map((stat, index) => (
             <motion.div
               key={stat.label}
               initial={{ scale: 0 }}
-              animate={inView ? { scale: 1 } : { scale: 0 }}
+              animate={{ scale: 1 }}
               transition={{
                 type: "spring",
                 stiffness: 120,
@@ -31,7 +28,7 @@ export default function CountSection() {
               }}
             >
               <div className="text-5xl sm:text-5xl md:text-6xl font-bold mb-2">
-                {inView ? <CountUp end={stat.value} duration={3} /> : 0}+
+                <CountUp end={stat.value} duration={4} />+
               </div>
               <h3 className="text-brand">{stat.label}</h3>
             </motion.div>
